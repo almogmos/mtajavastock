9ab2cba0cb66ca02b9fa38525caca9bf5f7175ba
almog moskovich <almogmos@gmail.com>
2014-11-12 17:41:05 +0200

    HomeWork #3: area, length of triangles, pow.

--- src/com/mta/javacourse/HelloServlet.java
+++ src/com/mta/javacourse/HelloServlet.java
@@ -2,24 +2,40 @@ package com.mta.javacourse;
 
 import java.io.IOException;
 import javax.servlet.http.*;
+import java.math.*;
 
 @SuppressWarnings("serial")
 public class HelloServlet extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws IOException {
 		resp.setContentType("text/html");
-		int num1;
-		int num2;
-		int num3;
-						
-		num1 = 4;
-		num2 = 3;
-		num3 = 7;
+
+		int num1 = 4;
+		int num2 = 3;
+		int num3 = 7;
 		
+		double pie = Math.PI;
+		double radius = 50;
+		double angle = 30;
+		double radianAngle = angle*pie/180;
+		int hypotenuseLength = 50;
+		double opposit = Math.sin(radianAngle)*hypotenuseLength;
+		int base = 20;
+		int exp = 13;
 		int result = (num3*num3)+(num3-num1-num2);
 		
+		double area = pie*Math.pow(radius, 2); 
+		
 		String resultStr = new String("<h1>Result of "+num3+"*"+num3+"+"+num3+"-"+num1+"-"+num2+"="+result+"</h1>");
-				
+		
+		String line1 = new String("<h1>calculation 1: Area of circle with radius "+radius+" is: "+area+"cm"+"</h1>");
+		String line2 = new String("<h1>calculation 2: Length of opposite where angle B is: "+opposit+"cm"+"</h1>");
+		String line3 = new String("<h1>calculation 3: Power of 20 with exp of 13 is: "+Math.pow(base, exp)+"</h1>");
+		
+		
 		resp.getWriter().println(resultStr);
+		resp.getWriter().println(line1);
+		resp.getWriter().println(line2);
+		resp.getWriter().println(line3);
 	}
 }
